d5a5baf14fceeeaf135977565c2791e42bac1cba
aromansson <ggsromansson@gmail.com>
2020-05-16 00:53:38 +0300

    1

--- src/Discriminant.java
+++ src/Discriminant.java
@@ -4,13 +4,13 @@ import java.io.IOException;
  * Created by roman on 7/2/16.
  */
 public class Discriminant {
-//    public static void main(String[] args) throws IOException {
-//
-//        System.out.println("Hello, it is a simple square equations calculator");
-//        DiscriminantEngine dis = new DiscriminantEngine();
-//        dis.addA();
-//        dis.addB();
-//        dis.addC();
-//        dis.calculate();
-//    }
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello, it is a simple square equations calculator");
+        DiscriminantEngine dis = new DiscriminantEngine();
+        dis.addA();
+        dis.addB();
+        dis.addC();
+        dis.calculate();
+    }
 }
--- src/Panell.java
+++ src/Panell.java
@@ -1,45 +1,45 @@
-import javax.swing.*;
-import java.awt.*;
-
-/**
- * Created by roman on 7/2/16.
- */
-public class Panell {
-    public static void main(String[] args) {
-        Panell gui = new Panell();
-        gui.go();
-    }
-
-    public void go() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.darkGray);
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        JButton button = new JButton("calculate");
-        JButton button1 = new JButton("exit");
-
-        JTextField ax2 = new JTextField();
-        JLabel ax2Lab = new JLabel();
-        JTextField bx = new JTextField();
-        JTextField c = new JTextField();
-        JTextField root1 = new JTextField();
-        JTextField root2 = new JTextField();
-
-        panel.add(button);
-        panel.add(button1);
-        panel.add(ax2);
-        panel.add(bx);
-        panel.add(c);
-        panel.add(root1);
-        panel.add(root2);
-
-        frame.getContentPane().add(BorderLayout.EAST, panel);
-        frame.setSize(200, 200);
-        frame.setVisible(true);
-    }
-    public void calc(){
-
-    }
-
-}
+//import javax.swing.*;
+//import java.awt.*;
+//
+///**
+// * Created by roman on 7/2/16.
+// */
+//public class Panell {
+//    public static void main(String[] args) {
+//        Panell gui = new Panell();
+//        gui.go();
+//    }
+//
+//    public void go() {
+//        JFrame frame = new JFrame();
+//        JPanel panel = new JPanel();
+//        panel.setBackground(Color.darkGray);
+//        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+//
+//        JButton button = new JButton("calculate");
+//        JButton button1 = new JButton("exit");
+//
+//        JTextField ax2 = new JTextField();
+//        JLabel ax2Lab = new JLabel();
+//        JTextField bx = new JTextField();
+//        JTextField c = new JTextField();
+//        JTextField root1 = new JTextField();
+//        JTextField root2 = new JTextField();
+//
+//        panel.add(button);
+//        panel.add(button1);
+//        panel.add(ax2);
+//        panel.add(bx);
+//        panel.add(c);
+//        panel.add(root1);
+//        panel.add(root2);
+//
+//        frame.getContentPane().add(BorderLayout.EAST, panel);
+//        frame.setSize(200, 200);
+//        frame.setVisible(true);
+//    }
+//    public void calc(){
+//
+//    }
+//
+//}
